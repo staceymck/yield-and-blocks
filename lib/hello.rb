0e300422cb786c289e2b6ac84835cff52628ab63
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-09 11:44:25 -0600

    commit tree c3bbc58c330c4202fec053e2409222539c823de6

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
-end
+def hello_t(array) #this method carries out the same functionality as #each; it iterates over each element in an array
+    if block_given?
+        i = 0
+        while i < array.length
+            yield(array[i])
+            i += 1
+        end
+        array #returns original array
+    else
+        puts "Hey! No block was given!"
+    end
+end 
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
+    end
+  end 
